b1ddc6056432b155cd75b8afea7e75c5d2fef7e3
mbrummelhuis <mbrummelhuis@gmail.com>
2024-08-13 14:17:26 +0200

    Flight mode takeoff

--- flight_modes/mode_takeoff/include/mode_takeoff.hpp
+++ flight_modes/mode_takeoff/include/mode_takeoff.hpp
@@ -3,50 +3,26 @@
 
 #include <px4_msgs/msg/offboard_control_mode.hpp>
 #include <px4_msgs/msg/trajectory_setpoint.hpp>
-#include <px4_msgs/msg/vehicle_control_mode.hpp>
 #include <px4_msgs/srv/vehicle_command.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <stdint.h>
-#include <px4_frame_transforms_lib/frame_transforms.h>
 
-#include <chrono>
-#include <iostream>
-#include <string>
-
-using namespace std::chrono;
-using namespace std::chrono_literals;
 using namespace px4_msgs::msg;
 
 class ModeTakeoff : public rclcpp::Node
 {
 public:
-	ModeTakeoff(std::string px4_namespace);
-
-	void switch_to_offboard_mode();
-	void arm();
+	ModeTakeoff();
 
 private:
-	enum class State{
-		init,
-		offboard_requested,
-		wait_for_stable_offboard_mode,
-		arm_requested,
-		armed
-	} state_;
-	uint8_t service_result_;
-	bool service_done_;
-	rclcpp::TimerBase::SharedPtr timer_;
+	float altitude_ = 1.0; // Takeoff altitude in meters (positive up)
 
 	rclcpp::Publisher<OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
 	rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
 	rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedPtr vehicle_command_client_;
 
-
 	void publish_offboard_position_mode();
 	void publish_trajectory_setpoint();
-	void request_vehicle_command(uint16_t command, float param1 = 0.0, float param2 = 0.0);
-	void response_callback(rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future);
-	void timer_callback(void);
+	void takeoff();
 };
 
 #endif
--- flight_modes/mode_takeoff/src/mode_takeoff.cpp
+++ flight_modes/mode_takeoff/src/mode_takeoff.cpp
@@ -41,59 +41,21 @@
 
 #include <px4_msgs/msg/offboard_control_mode.hpp>
 #include <px4_msgs/msg/trajectory_setpoint.hpp>
-#include <px4_msgs/msg/vehicle_control_mode.hpp>
 #include <px4_msgs/srv/vehicle_command.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <stdint.h>
-#include <mode_takeoff.hpp>
 
-#include <chrono>
-#include <iostream>
-#include <string>
+#include "mode_takeoff.hpp"
 
-using namespace std::chrono;
-using namespace std::chrono_literals;
 using namespace px4_msgs::msg;
 
-
-
-ModeTakeoff::ModeTakeoff(std::string px4_namespace) :
+ModeTakeoff::ModeTakeoff() :
 		Node("mode_takeoff"),
-		state_{State::init},
-		service_result_{0},
-		service_done_{false},
-		offboard_control_mode_publisher_{this->create_publisher<OffboardControlMode>(px4_namespace+"in/offboard_control_mode", 10)},
-		trajectory_setpoint_publisher_{this->create_publisher<TrajectorySetpoint>(px4_namespace+"in/trajectory_setpoint", 10)},
-		vehicle_command_client_{this->create_client<px4_msgs::srv::VehicleCommand>(px4_namespace+"vehicle_command")}
+		offboard_control_mode_publisher_{this->create_publisher<OffboardControlMode>("/offboard_control_mode", 10)},
+		trajectory_setpoint_publisher_{this->create_publisher<TrajectorySetpoint>("/trajectory_setpoint", 10)}
 {
 	RCLCPP_INFO(this->get_logger(), "Starting Takeoff mode");
-	RCLCPP_INFO_STREAM(this->get_logger(), "Waiting for " << px4_namespace << "vehicle_command service");
-	while (!vehicle_command_client_->wait_for_service(1s)) {
-		if (!rclcpp::ok()) {
-			RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
-			return;
-		}
-		RCLCPP_INFO(this->get_logger(), "service not available, waiting again...");
-	}
 
-	timer_ = this->create_wall_timer(100ms, std::bind(&ModeTakeoff::timer_callback, this));
-}
-
-/**
- * @brief Send a command to switch to offboard mode
- */
-void ModeTakeoff::switch_to_offboard_mode(){
-	RCLCPP_INFO(this->get_logger(), "requesting switch to Offboard mode");
-	request_vehicle_command(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
-}
-
-/**
- * @brief Send a command to Arm the vehicle
- */
-void ModeTakeoff::arm()
-{
-	RCLCPP_INFO(this->get_logger(), "requesting arm");
-	request_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
+	takeoff();
 }
 
 /**
@@ -115,138 +77,28 @@ void ModeTakeoff::publish_offboard_position_mode()
 /**
  * @brief Publish a trajectory setpoint
  *        For this example, it sends a trajectory setpoint to make the
- *        vehicle hover at 5 meters with a yaw angle of 180 degrees.
+ *        vehicle hover at 1 meters.
  */
 void ModeTakeoff::publish_trajectory_setpoint()
 {
+	float z_position = -1.0*altitude_;
 	TrajectorySetpoint msg{};
-	msg.position = {0.0, 0.0, -1.0};
+	msg.position = {0.0, 0.0, z_position};
 	//msg.yaw = 0; // [-PI:PI]
 	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
 	trajectory_setpoint_publisher_->publish(msg);
 }
 
-/**
- * @brief Publish vehicle commands
- * @param command   Command code (matches VehicleCommand and MAVLink MAV_CMD codes)
- * @param param1    Command parameter 1
- * @param param2    Command parameter 2
- */
-void ModeTakeoff::request_vehicle_command(uint16_t command, float param1, float param2)
-{
-	auto request = std::make_shared<px4_msgs::srv::VehicleCommand::Request>();
-
-	VehicleCommand msg{};
-	msg.param1 = param1;
-	msg.param2 = param2;
-	msg.command = command;
-	msg.target_system = 1;
-	msg.target_component = 1;
-	msg.source_system = 1;
-	msg.source_component = 1;
-	msg.from_external = true;
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-	request->request = msg;
-
-	service_done_ = false;
-	auto result = vehicle_command_client_->async_send_request(request, std::bind(&ModeTakeoff::response_callback, this,
-                           std::placeholders::_1));
-	RCLCPP_INFO(this->get_logger(), "Command send");
-}
-
-void ModeTakeoff::timer_callback(void){
-	
-	static uint8_t num_of_steps = 0;
-
-	// offboard_control_mode needs to be paired with trajectory_setpoint
+void ModeTakeoff::takeoff(){
 	publish_offboard_position_mode();
 	publish_trajectory_setpoint();
-
-	switch (state_)
-	{
-	case State::init :
-		switch_to_offboard_mode();
-		state_ = State::offboard_requested;
-		break;
-	case State::offboard_requested :
-		if(service_done_){
-			if (service_result_==0){
-				RCLCPP_INFO(this->get_logger(), "Entered offboard mode");
-				state_ = State::wait_for_stable_offboard_mode;				
-			}
-			else{
-				RCLCPP_ERROR(this->get_logger(), "Failed to enter offboard mode, exiting");
-				rclcpp::shutdown();
-			}
-		}
-		break;
-	case State::wait_for_stable_offboard_mode :
-		if (++num_of_steps>10){
-			arm();
-			state_ = State::arm_requested;
-		}
-		break;
-	case State::arm_requested :
-		if(service_done_){
-			if (service_result_==0){
-				RCLCPP_INFO(this->get_logger(), "vehicle is armed");
-				state_ = State::armed;
-			}
-			else{
-				RCLCPP_ERROR(this->get_logger(), "Failed to arm, exiting");
-				rclcpp::shutdown();
-			}
-		}
-		break;
-	default:
-		break;
-	}
 }
-
-void ModeTakeoff::response_callback(
-      rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future) {
-    auto status = future.wait_for(1s);
-    if (status == std::future_status::ready) {
-	  auto reply = future.get()->reply;
-	  service_result_ = reply.result;
-      switch (service_result_)
-		{
-		case reply.VEHICLE_CMD_RESULT_ACCEPTED:
-			RCLCPP_INFO(this->get_logger(), "command accepted");
-			break;
-		case reply.VEHICLE_CMD_RESULT_TEMPORARILY_REJECTED:
-			RCLCPP_WARN(this->get_logger(), "command temporarily rejected");
-			break;
-		case reply.VEHICLE_CMD_RESULT_DENIED:
-			RCLCPP_WARN(this->get_logger(), "command denied");
-			break;
-		case reply.VEHICLE_CMD_RESULT_UNSUPPORTED:
-			RCLCPP_WARN(this->get_logger(), "command unsupported");
-			break;
-		case reply.VEHICLE_CMD_RESULT_FAILED:
-			RCLCPP_WARN(this->get_logger(), "command failed");
-			break;
-		case reply.VEHICLE_CMD_RESULT_IN_PROGRESS:
-			RCLCPP_WARN(this->get_logger(), "command in progress");
-			break;
-		case reply.VEHICLE_CMD_RESULT_CANCELLED:
-			RCLCPP_WARN(this->get_logger(), "command cancelled");
-			break;
-		default:
-			RCLCPP_WARN(this->get_logger(), "command reply unknown");
-			break;
-		}
-      service_done_ = true;
-    } else {
-      RCLCPP_INFO(this->get_logger(), "Service In-Progress...");
-    }
-  }
 
 int main(int argc, char *argv[])
 {
 	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 	rclcpp::init(argc, argv);
-	rclcpp::spin(std::make_shared<ModeTakeoff>("/fmu/"));
+	rclcpp::spin(std::make_shared<ModeTakeoff>());
 
 	rclcpp::shutdown();
 	return 0;
