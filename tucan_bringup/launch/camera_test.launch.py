a3ccb72a3ded580ef99d7c7136696edee0c7b376
Anton Lang <oesterreich25euro@gmail.com>
2024-09-10 16:25:07 +0200

    ?

--- tucan_bringup/launch/camera_test.launch.py
+++ tucan_bringup/launch/camera_test.launch.py
@@ -51,7 +51,7 @@ def generate_launch_description():
 
     # Add all the actions
     ld.add_action(down_camera_driver_node)
-    ld.add_action(front_camera_driver_node)
+    #ld.add_action(front_camera_driver_node)
     ld.add_action(offboard_handler)
 
     return ld
