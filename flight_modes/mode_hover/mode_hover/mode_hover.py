f2eaae6f68ed6485383b556ecb6a42f2aac606d7
Anton Lang <oesterreich25euro@gmail.com>
2024-09-11 13:36:27 +0200

    fix

--- flight_modes/mode_hover/mode_hover/mode_hover.py
+++ flight_modes/mode_hover/mode_hover/mode_hover.py
@@ -35,8 +35,8 @@ class ModeHover(Node):
         
         self.state_subscriber_ = self.create_subscription(tucan_msgs.Mode, "/active_mode_id", self.state_callback, 10)
         self.vehicle_odom_subscriber_ = self.create_subscription(px4_msgs.VehicleOdometry, "fmu/out/vehicle_odometry", self.vehicle_odom_callback, qos_profile)
-        self.yaw_subscriber = self.create_subscription(std_msgs.Float32, "mode_hover/desired_yaw", self.desired_yaw_callback)
-        self.alt_subscriber = self.create_subscription(std_msgs.Float32, "mode_hover/desired_altitude", self.desired_alt_callback)
+        self.yaw_subscriber = self.create_subscription(std_msgs.Float32, "mode_hover/desired_yaw", self.desired_yaw_callback, 5)
+        self.alt_subscriber = self.create_subscription(std_msgs.Float32, "mode_hover/desired_altitude", self.desired_alt_callback, 5)
 
         self.mode_status_publisher_ = self.create_publisher(tucan_msgs.ModeStatus, "/mode_status", 10)
         
